92422b07b4f6814491cf68212bbd3b5ba26b3dd5
Nichole <nicholemcgrew@gmail.com>
2023-11-07 21:59:00 -0600

    updated CSS

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+// <!-- JavaScript to control the modal -->
+   
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("book-now");
+
+        // Get the close button
+        var closeBtn = document.getElementById("closeModal");
+
+        // When the user clicks the button, open the modal
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on the close button, close the modal
+        closeBtn.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks outside the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+
+
 document.getElementById('book-now').addEventListener('click', function() {
     // Add your booking functionality here
     alert('Booking feature coming soon!');
