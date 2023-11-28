6b9f3dde4a89b90fd3f3f7550bda5838b277c846
Nichole <nicholemcgrew@gmail.com>
2023-11-28 09:15:46 -0600

    removed styling on headerto fix hamburger menu

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = () => {
+menu.addEventListener('click', () => {
     menu.classList.toggle('fas-times');
     navbar.classList.toggle('active');
-}
+});
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
+window.addEventListener('scroll', () => {
+    menu.classList.remove('fas-times');
     navbar.classList.remove('active');
-}
+});
 
 // Typed js
 
